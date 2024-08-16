27a981bec96a76072062af3477454efe2f9bdc5d
JunBe <wnsqowid0901@gmail.com>
2024-08-17 05:09:43 +0900

    역할,구현 주입 분리

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -2,12 +2,12 @@ package hello.core.member;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+       /* MemberService memberService = new MemberServiceImpl(memberRepository);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
-        System.out.println("find Member = " + findMember.getName());
+        System.out.println("find Member = " + findMember.getName());*/
     }
 }
