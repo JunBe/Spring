58c9273c7f973064fb1d22464775f7d37b9b7386
JunBe <wnsqowid0901@gmail.com>
2024-08-21 23:41:49 +0900

    빈 스코프

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.FilterType;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
-@Configuration
+//@Configuration
 @ComponentScan(
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
